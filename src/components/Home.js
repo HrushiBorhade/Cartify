7fd744347529bfbfa05dceac4b1aefc53d7a9bee
Hrushikesh Borhade <hrushiborhade123@gmail.com>
2023-03-04 22:56:34 +0530

    home page ui layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { CartState } from "../context/Context";
+import SingleProduct from "../components/SingleProduct";
+import "./styles.css";
 function Home() {
   const {
     state: { products },
   } = CartState();
   console.log(products);
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      {/* <Filters /> */}
+      <div className="productContainer">
+        {products.map((prod) => {
+          return <SingleProduct prod={prod} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Home;

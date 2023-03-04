9720f50c343331388df0a293ca04129d5d67f8ed
Hrushikesh Borhade <hrushiborhade123@gmail.com>
2023-03-04 22:43:41 +0530

    created cartContext and cartReducer, initialised product fake data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
-
+import { CartState } from "../context/Context";
 function Home() {
+  const {
+    state: { products },
+  } = CartState();
+  console.log(products);
   return <div>Home</div>;
 }
 
---
+++ src/context/Context.js
@@ -0,0 +1,29 @@
+import { createContext, useReducer, useContext } from "react";
+import faker from "faker";
+import { cartReducer } from "./Reducer";
+const Cart = createContext();
+
+const Context = ({ children }) => {
+  const products = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.imageUrl(),
+    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+    fastDelivery: faker.datatype.boolean(),
+    ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+  }));
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+
+  return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>;
+};
+
+export const CartState = () => {
+  return useContext(Cart);
+};
+
+export default Context;
